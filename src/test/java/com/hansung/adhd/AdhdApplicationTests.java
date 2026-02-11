28937bd29a618468b8e90bc426b296c7ff67863a
ldzb <ldzb1123@gmail.com>
2026-02-11 18:00:37 +0900

    feat: DB 연동

---
+++ src/test/java/com/hansung/adhd/AdhdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansung.adhd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdhdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
